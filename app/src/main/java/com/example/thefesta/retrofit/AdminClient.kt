8ca19bdb8112780f159eb02618b7bda1b27c6c29
pueser <hwanghyejin932@gmail.com>
2023-12-27 08:27:05 +0900

    2023.12.27 admin adminActivity and admin files correction

--- app/src/main/java/com/example/thefesta/retrofit/AdminClient.kt
+++ app/src/main/java/com/example/thefesta/retrofit/AdminClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object AdminClient {
-    private const val BASE_URL = "http://192.168.4.16:9090/"
+    private const val BASE_URL = "http://172.30.1.99:9090/"
 
     private val gson = GsonBuilder()
         .setLenient()
