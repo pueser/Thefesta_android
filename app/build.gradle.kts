b6079d2df0406519256b85d74f160aba1db7058a
ekdlsdlsep <dain7362@gmail.com>
2023-12-26 12:26:10 +0900

    2023/12/26 SDI 회원 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,14 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.github.bumptech.glide:glide:4.13.2")
 
-    implementation("com.google.android.material:material:1.4.0")
-
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(files("libs/libDaumMapAndroid.jar"))
+
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("com.github.bumptech.glide:compiler:4.12.0")
+
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("com.tbuonomo:dotsindicator:4.2")
+
+    implementation("com.google.android.material:material:1.4.0")
 }
